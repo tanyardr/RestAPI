43de67ab26dea62616c63629cdf91484dcdfa4c8
taniadr <taniadiasr@gmail.com>
2020-01-11 21:48:48 -0300

    added simple nodejs server to project 'OnlyNodePlusHttp'

--- OnlyNodePlusHttp/nodeWebSev.js
+++ OnlyNodePlusHttp/nodeWebSev.js
@@ -1,16 +1,14 @@
 //simple node server for communication test
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require('http');
+var fs = require('fs');
+
+const port = 8080;
+
+http.createServer(function (req, res) { 
+	req.writeHead(200, {'Content-Type' : 'text/html'});
 
 
-const port = 3000;
-server = express();
-
-
-server.listen(process.env.PORT || port, function (){
-	console.log('Listening on');
-})
-
+}).listen(8080);
 
 server.get('/', function (req, res) {
   //res.sendFile(__dirname + '/index.html');
