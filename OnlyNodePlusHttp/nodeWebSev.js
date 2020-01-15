5a1d1c4b03000dcb3881a556643bded96e434e9b
taniadr <taniadiasr@gmail.com>
2020-01-15 00:25:19 -0300

    Flask API

--- OnlyNodePlusHttp/nodeWebSev.js
+++ OnlyNodePlusHttp/nodeWebSev.js
@@ -1,17 +1,16 @@
 //simple node server for communication test
 const http = require('http');
-var fs = require('fs');
+const fs = require('fs');
 
 const port = 8080;
 
 http.createServer(function (req, res) { 
-	req.writeHead(200, {'Content-Type' : 'text/html'});
+	fs.readFile('demoDb.html', function (err, data) {
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+		res.write(data)
+		res.end()
+	});
 
+}).listen(port);
 
-}).listen(8080);
-
-server.get('/', function (req, res) {
-  //res.sendFile(__dirname + '/index.html');
-  res.render('index', {});
-});
 
