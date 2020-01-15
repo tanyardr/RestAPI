4177af73930cab97b1c90c4ed027c2628888092c
taniadr <taniadiasr@gmail.com>
2020-01-15 01:31:37 -0300

    yey

--- PyProjects/api/api.py
+++ PyProjects/api/api.py
@@ -1,22 +1,33 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-name = ['Monja Coen', 'Fernando Pessoa', 'Ferreira Gular']
-booksFrom = [1, 2, 3]
-booksFrom[1] = ['Zen Para Distraidos', 'A monja e o Professor']
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
 
-def listBooks():
-    for i in booksFrom[1]:
-        print(booksFrom(i))
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This is a prototype API for distant reading.</p>"
+    return "'<h1>Distant Reading Archive</h1><p>This is a prototype API for distant reading.</p>'"
 
-@app.route('/authors/<name>')
-def authors(name):
-    return '<h1>Books %s</h1>' % listBooks  
-  
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
 app.run()
