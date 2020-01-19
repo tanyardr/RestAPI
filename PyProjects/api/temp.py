c861d2c8c0fe178201eb8efb3e688a914f877975
taniadr <taniadiasr@gmail.com>
2020-01-19 19:59:58 -0300

    git issues fixed

---
+++ PyProjects/api/temp.py
@@ -0,0 +1,24 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+name = ['Monja Coen', 'Fernando Pessoa', 'Ferreira Gular']
+booksFrom = [1, 2, 3]
+booksFrom[1] = ['Zen Para Distraidos', 'A monja e o Professor']
+
+def listBooks():
+    for i in booksFrom[1]:
+        print(booksFrom(i))
+
+@app.route('/authors/<name>')
+def authors(name):
+    return '<h1>Books %s</h1>' % listBooks 
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This is a prototype API for distant reading.</p>"
+
+
+  
+app.run()
